20f0080b033015b1028741c1911870335fea004a
sandeep <saisandeeppotnuru2003@gmail.com>
2026-05-21 11:15:21 +0530

    java first fundamentals

---
+++ src/com/fundamentals/First_fundamentals.java
@@ -0,0 +1,23 @@
+package com.fundamentals;
+
+public class First_fundamentals {
+	byte a=45;
+	short b=154;
+	int age =1524;
+	long phone= 9440_2746_77l;
+	float index =15.45f;
+	double PI =3.14;
+	boolean bool = true;
+	char gender='M';
+	public static void main(String[] args) {
+		First_fundamentals f = new First_fundamentals();
+		System.out.println(f.a);
+		System.out.println(f.b);
+		System.out.println(f.age);
+		System.out.println(f.phone);
+		System.out.println(f.index);
+		System.out.println(f.PI);
+		System.out.println(f.bool);
+		System.out.println(f.gender);
+	}
+}
