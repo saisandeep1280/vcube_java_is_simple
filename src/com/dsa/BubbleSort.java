065f030da659952329ef51c234a5011204457142
sandeep <saisandeeppotnuru2003@gmail.com>
2026-08-06 10:39:22 +0530

    Continue Java exercises

--- src/com/dsa/BubbleSort.java
+++ src/com/dsa/BubbleSort.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		
 		int[] arr = { 5, 2, 9, 1, 5, 6 };
 	     for (int i=0;i<arr.length-1;i++) {
 	    	 for (int j=0;j<arr.length-1-i;j++) {
--- src/com/patterns/Star2.java
+++ src/com/patterns/Star2.java
@@ -1,6 +1,6 @@
 package com.patterns;
 
-public class ReverseStar2 {
+public class Star2 {
 
 	public static void main(String[] args) {
 		for(int i=6;i>=0;i--) {
